07d993e1c44ed1a5b22d77e8a0fbb00a08ed9ebf
LegendaryGeek <timothydaley4@gmail.com>
2019-10-15 18:54:09 -0400

    Hacktoberfest commit 2 for Kenzies mod

---
+++ src/main/java/kenzie/things/items/HacktoberfestItem2.java
@@ -0,0 +1,13 @@
+package kenzie.things.items;
+
+import net.minecraft.item.Item;
+
+public class HacktoberfestItem2 extends Item {
+
+	public HacktoberfestItem2(Properties properties) {
+		super(properties);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
